bcc73efe5eb1eb972c29f9c4ac8b86f0a3a2a41a
istakhar1 <istakhar054@gmail.com>
2023-04-14 22:59:59 +0530

    new library

---
+++ library/src/main/java/com/example/library/Library.java
@@ -0,0 +1,9 @@
+package com.example.library;
+import  android.content.Context;
+import android.widget.Toast;
+
+public class Library {
+    public  static  void simpleLibrary(Context context){
+        Toast.makeText(context, "Hello Wigzo", Toast.LENGTH_SHORT).show();
+    }
+}
